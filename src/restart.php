6b1e83833858f0aa436bf5a8f7580680565ef137
Djurre <djurreaikema@gmail.com>
2024-06-09 11:40:01 +0200

    SonarQube - Medium Severity - restart.php

--- src/restart.php
+++ src/restart.php
@@ -1,12 +1,19 @@
 <?php
 
+use Core\Database;
+
+$mysqli = new Database();
+
 session_start();
 
 $_SESSION['board'] = [];
 $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
 $_SESSION['player'] = 0;
 
-$db = include_once 'Database.php';
+try {
+    $db = $mysqli->connect();
+} catch (Exception $e) {
+}
 $db->prepare('INSERT INTO games VALUES ()')->execute();
 $_SESSION['game_id'] = $db->insert_id;
 
