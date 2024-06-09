869e0e73fdc05fcb2ca611ee1f5ed777aa76602a
Djurre <djurreaikema@gmail.com>
2024-06-09 11:47:09 +0200

    SonarQube - Medium Severity - Util.php

--- src/Util.php
+++ src/Util.php
@@ -6,50 +6,55 @@ $GLOBALS['OFFSETS'] = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1]];
 
 class Util
 {
-    function isNeighbour($a, $b)
+    function isNeighbour($a, $b): bool
     {
         $a = explode(',', $a);
         $b = explode(',', $b);
-        if ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) return true;
-        if ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) return true;
-        if ($a[0] + $a[1] == $b[0] + $b[1]) return true;
+        if (
+            ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1)
+            || ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1)
+            || ($a[0] + $a[1] == $b[0] + $b[1])
+        ) {
+            return true;
+        }
         return false;
     }
 
-    function hasNeighBour($a, $board)
+    function hasNeighbour($a, $board): bool
     {
         foreach (array_keys($board) as $b) {
-            if (isNeighbour($a, $b)) return true;
+            if ($this->isNeighbour($a, $b)) return true;
         }
+        return false;
     }
 
-    function neighboursAreSameColor($player, $a, $board)
+    function neighboursAreSameColor($player, $a, $board): bool
     {
         foreach ($board as $b => $st) {
             if (!$st) continue;
             $c = $st[count($st) - 1][0];
-            if ($c != $player && isNeighbour($a, $b)) return false;
+            if ($c != $player && $this->isNeighbour($a, $b)) return false;
         }
         return true;
     }
 
-    function len($tile)
+    function len($tile): int
     {
         return $tile ? count($tile) : 0;
     }
 
-    function slide($board, $from, $to)
+    function slide($board, $from, $to): bool
     {
-        if (!hasNeighBour($to, $board)) return false;
-        if (!isNeighbour($from, $to)) return false;
+        if (!$this->hasNeighBour($to, $board)) return false;
+        if (!$this->isNeighbour($from, $to)) return false;
         $b = explode(',', $to);
         $common = [];
         foreach ($GLOBALS['OFFSETS'] as $pq) {
             $p = $b[0] + $pq[0];
             $q = $b[1] + $pq[1];
-            if (isNeighbour($from, $p . "," . $q)) $common[] = $p . "," . $q;
+            if ($this->isNeighbour($from, $p . "," . $q)) $common[] = $p . "," . $q;
         }
-        if (!$board[$common[0]] && !$board[$common[1]] && !$board[$from] && !$board[$to]) return false;
-        return min(len($board[$common[0]]), len($board[$common[1]])) <= max(len($board[$from]), len($board[$to]));
+        return !(!$board[$common[0]] && !$board[$common[1]] && !$board[$from] && !$board[$to])
+            && min($this->len($board[$common[0]]), $this->len($board[$common[1]])) <= max($this->len($board[$from]), $this->len($board[$to]));
     }
 }
